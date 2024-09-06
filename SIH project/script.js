7e1eb3cefc96c50db8bf8982611fc099ccc7f7a7
Divyansh Narayan Mishra <162825118+DivyanshNM@users.noreply.github.com>
2024-09-06 06:57:22 +0530

    Delete SIH project directory

--- SIH project/script.js
+++
@@ -1,7 +0,0 @@
-document.getElementById('showChatbotBtn').addEventListener('click', function() {
-    document.getElementById('chatbot').style.display = 'block';
-});
-
-document.getElementById('closeChatbotBtn').addEventListener('click', function() {
-    document.getElementById('chatbot').style.display = 'none';
-});
